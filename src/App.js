51c7b01d4ba1c19cca29c072aa510c435dfca0c7
Kate Glushkova <weirdis1@gmail.com>
2023-12-07 12:02:02 +0200

    coordonates set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import { randColor, randInt } from './helpers/generators';
 import Flake from "./flake/component"
 
 function App() {
   return (
     <div className="App">
-      <Flake />
+      <Flake 
+        size={randInt(20, 40)} 
+        color={randColor()}
+        top={0}
+        left={randInt(0, 100)}  
+      />
     </div>
   );
 }
--- src/flake/component.js
+++ src/flake/component.js
@@ -1,14 +1,26 @@
 import './style.scss';
-import { randInt, randColor } from '../helpers/generators';
+import {useState} from 'react'
 
+//HW1: Make sure the component detects the moment it reaches the bottom
+//HW2: When the component reaches the bottom it should remove itself from the React DOM
 const Flake = ({
-        size = randInt(20,40),
-        color = randColor()
+        size  = 0,
+        color = 0,
+        top   = 0,
+        left  = 0,
     }) => {
+
+    const [_top, setTop] = useState(top)
+    setTimeout(() => {
+        setTop(_top + 10)
+        console.log("update executed")
+    }, 500)
 
     const style = {
         '--size': `${size}px`,
-        '--color': color
+        '--color': color,
+        '--top': `${_top}%`,
+        '--left': `${left}%`,
     }
 
     return (
